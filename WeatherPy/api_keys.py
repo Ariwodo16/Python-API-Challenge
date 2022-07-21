f79570a5c71974a80e8fa62cdb92007a39cb0727
Ariwodo16 <ariwodomichael6@gmail.com>
2022-07-21 01:38:07 -0400

    Required Materials

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "41ee7986ee745d683aaa6e6628fc73c3"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyB0FNVaFABgObeDZEZQiKfE8Hwe0Ov8M4w"
